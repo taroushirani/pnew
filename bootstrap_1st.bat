b9340ddf027cf94d22f53454737f7a537956b6b2
Tarou Shirani <taroushirani@gmail.com>
2020-09-10 01:01:20 +0900

    Change to use start /wait when calling msys2.exe

--- bootstrap_1st.bat
+++ bootstrap_1st.bat
@@ -19,8 +19,6 @@ set msys2_archive_path=%tmp_dir%\msys2.tar.xz
 set dest_root=%~dp0
 set msys2_dest_dir=%dest_root%
 
-set timeout_sec=60
-
 rem main routine
 rem Remove old directories
 call :check_directory %tmp_dir%
@@ -60,10 +58,7 @@ echo Move python to /opt of MSYS2.
 move %tmp_dir%\%winpython_python_relative_path% %msys2_dest_dir%\msys64\opt\python
 
 echo Initialize MSYS2.
-call %msys2_dest_dir%\msys64\msys2.exe /dev/null
-
-echo Wait %timeout_sec% seconds to finish initialization of MSYS2.
-timeout /t %timeout_sec% /nobreak >nul
+start /wait %msys2_dest_dir%\msys64\msys2.exe nul
 
 echo Add /opt/python to PATH.
 echo export PATH=/opt/python:/opt/python/Scripts:$PATH >> msys64\etc\bash.bashrc
