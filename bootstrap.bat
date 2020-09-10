e7bdcbf91ad70252babbff4a65cb7d0d38c81188
Tarou Shirani <taroushirani@gmail.com>
2020-09-10 10:39:55 +0900

    Minor fix.

--- bootstrap.bat
+++ bootstrap.bat
@@ -17,5 +17,7 @@ if %errorlevel% neq 0 (
 )
 
 echo Bootstrap finished.
+echo You can close this window and launch msys64/msys2.exe to use NNSVS from Un*x userland,
+echo or you can continue to use cmd.exe if have expertise in NNSVS and Windows.
 
 @echo on
